f532c5349e42c5bd50b9403af4e635ce2ed3ef0c
Kevin Horton <kevin01@kilohotel.com>
2014-12-21 19:37:24 -0500

    ?

--- python/exif_write.py
+++ python/exif_write.py
@@ -1,13 +1,14 @@
 #! /sw/bin/python2.7
+# -*- coding: latin-1 -*-
 
 import argparse
 
 parser = argparse.ArgumentParser(description='Update JPGs using exiftool.')
 parser.add_argument('-l', dest='latlong', 
-  help='optional latitude and longitude, in format "lat,long", with N latitude positive, and E longitude positive')
+  help='optional latitude and longitude, in format "lat, long", with N latitude positive, and E longitude positive')
 parser.add_argument('-t', dest='time_delta',
   help='apply time_delta to camera time when comparing against GPS log. Format "+H:MM:SS"')
-parser.add_argument('JPGs', nargs='+',
+parser.add_argument('JPGs', nargs='*', default="*.JPGs", 
   help='List of JPG files to process')
 
 args = parser.parse_args()
@@ -30,7 +31,10 @@ if args.latlong:
 	else:
 		long*=-1
 		EW="W"
-	print "lat=%s %s" % (lat,NS)
-	print "long=%s %s" % (long, EW)
+	# print "lat=%s %s" % (lat,NS)
+	# print "long=%s %s" % (long, EW)
+
+	command = "exiftool −overwrite_original_in_place -P -GPSMapDatum=WGS-84 -gps:GPSLatitude=%s -gps:GPSLatitudeRef=%s -gps:GPSLongitude=%s -gps:GPSLongitudeRef=%s -xmp:GPSLatitude='%s %s' -xmp:GPSLongitude='%s %s' -xmp:GPSMapDatum='WGS-84' -xmp:GPSVersionID='2.2.0.0' %s" % (lat, NS, long, EW, lat, NS, long, EW, " ".join(args.JPGs))
+	print command
 else:
 	print "latLong not defined"
