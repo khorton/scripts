6c7b76792ec13d7a44525e7567aa51271b2b14bc
Kevin Horton <kevin01@kilohotel.com>
2015-01-11 18:22:57 -0500

    WIP.  Works so far.

--- python/gl2joomla.py
+++ python/gl2joomla.py
@@ -5,7 +5,7 @@
 import MySQLdb
 import sys
 import csv
-import re
+from bs4 import BeautifulSoup
 
 db=MySQLdb.connect(user='root',passwd="Yarmouth2",db="rv8_merge")
 c=db.cursor()
@@ -17,35 +17,110 @@ c=db.cursor()
 #     (sid, num, iid) = row
 #     print sid, num, iid
 
-# sid = 2015010420254995
-sid = 20141220235836599 # no images
-c.execute("SELECT ai_img_num, ai_filename FROM gl_article_images WHERE ai_sid=%s", sid)
-img_array = c.fetchmany(20)
-if c.rownumber > 0:
-    for row in img_array:
-        ai_img_num, ai_filename = row
-        print ai_img_num, ai_filename
-else:
-    print "no images"
+# sid = 2015010420254995 # with images
+# sid = 20141220235836599 # no images
+# c.execute("SELECT ai_img_num, ai_filename FROM gl_article_images WHERE ai_sid=%s", sid)
+# img_array = c.fetchmany(20)
+# if c.rownumber > 0:
+#     for row in img_array:
+#         ai_img_num, ai_filename = row
+#         print ai_img_num, ai_filename
+# else:
+#     print "no images"
 
 
-# c.execute("SELECT sid, uid, date, title, introtext, bodytext, hits, comments FROM gl_stories")
-# story_array = c.fetchmany(2000)
-# n = 1
-# for row in story_array:
-#     sid, uid, date, title, introtext, bodytext, hits, comments = row
-#     c.execute("SELECT tid, id from gl_topic_assignments WHERE id = %s", sid)
-#     tid, id = c.fetchone()
-#     print sid
-#     print id
-#     print tid
-#     print uid
-#     print date
-#     print title
-#     print introtext
-#     print "Body Text =", bodytext
-#     print "Hits =", hits
-#
-#     n -= 1
-#     if n < 1:
-#         sys.exit()
+def parse_all():
+    c.execute("SELECT sid, uid, date, title, introtext, bodytext, hits, comments FROM gl_stories")
+    story_array = c.fetchmany(2000)
+    n = 4
+    for row in story_array:
+        sid, uid, date, title, introtext, bodytext, hits, comments = row
+    
+        # get topic id
+        c.execute("SELECT tid, id from gl_topic_assignments WHERE id = %s", sid)
+        tid, id = c.fetchone()
+    
+        # get story images, if any
+        c.execute("SELECT ai_img_num, ai_filename FROM gl_article_images WHERE ai_sid=%s", id)
+        img_array = c.fetchmany(20)
+        if c.rownumber > 0:
+            print "Story Images:"
+            for row in img_array:
+                ai_img_num, ai_filename = row
+                print ai_img_num, ai_filename
+        else:
+            print "no images"
+    
+        print "Story ID=", sid
+        # print "ID =", id
+        # print "Topic=", tid
+        # print "uid=", uid
+        print "Story Date/Time=", date
+        print "Title=", title
+        print "Intro Text=", introtext
+        # print "Body Text =", bodytext
+        # print "Hits =", hits
+    
+        soup = BeautifulSoup(introtext,"html5lib")
+        print "image html:"
+        print soup.find_all('img')
+    
+        print "Src:", soup.img['src']
+        print "Width:", soup.img['width']
+        print "Height:", soup.img['height']
+        print "Alignment:", soup.img['align']
+        print "Alt Text:", soup.img['alt']
+        print "==============================\n=============================="
+        # Typical pattern if image is hardcoded:
+        # <img width="300" height="132" align="right" src="http://www.kilohotel.com/rv8/images/articles/20030507210918708_1.jpg" alt="">
+
+
+        n -= 1
+        if n < 1:
+            sys.exit()
+
+
+def parse_one(sid):
+    c.execute("SELECT sid, uid, date, title, introtext, bodytext, hits, comments FROM gl_stories WHERE sid=%s", sid)
+    sid, uid, date, title, introtext, bodytext, hits, comments = c.fetchone()
+
+    # get topic id
+    c.execute("SELECT tid, id from gl_topic_assignments WHERE id = %s", sid)
+    tid, id = c.fetchone()
+
+    # get story images, if any
+    c.execute("SELECT ai_img_num, ai_filename FROM gl_article_images WHERE ai_sid=%s", id)
+    img_array = c.fetchmany(20)
+    if c.rownumber > 0:
+        print "Story Images:"
+        for row in img_array:
+            ai_img_num, ai_filename = row
+            print ai_img_num, ai_filename
+    else:
+        print "no images"
+
+    print "Story ID=", sid
+    # print "ID =", id
+    # print "Topic=", tid
+    # print "uid=", uid
+    print "Story Date/Time=", date
+    print "Title=", title
+    print "Intro Text=", introtext
+    # print "Body Text =", bodytext
+    # print "Hits =", hits
+
+    soup = BeautifulSoup(introtext,"html5lib")
+    print "image html:"
+    print soup.find_all('img')
+
+    print "Src:", soup.img['src']
+    print "Width:", soup.img['width']
+    print "Height:", soup.img['height']
+    print "Alignment:", soup.img['align']
+    print "Alt Text:", soup.img['alt']
+    print "==============================\n=============================="
+    # Typical pattern if image is hardcoded:
+    # <img width="300" height="132" align="right" src="http://www.kilohotel.com/rv8/images/articles/20030507210918708_1.jpg" alt="">
+
+# parse_all()
+parse_one(2015010420254995)
