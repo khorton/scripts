f768ad6101c88d59b1f8d72ad1cd4acc2457183a
Kevin Horton <kevin01@kilohotel.com>
2015-01-14 06:15:58 -0500

    Add notes.  Add code to twiddle image file paths for old hard coded articles, without image thumbnails.

--- python/gl2joomla.py
+++ python/gl2joomla.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python
+"""Export Geeklog CMS database to CSV, after twiddling image tags to make them 
+compatible with Joomla's image storage location, and the NoNumber Modal
+image pop-up extension.
+"""
+
+###############################################################################
+#                   
+# To Do:
+#  1. Insert publication date as first para in body, as the Content Uploader
+#     extension does not import publication date.
+#  2. Write function to extract publication date from body via MySQL calls,
+#     and insert in appropriate table.
+#  3. Write code to insert Modal class declaration into old GL hard-coded
+#     image thumbnail tags.
+#  4. Write CSV export function.
+#  5. Write input arguement parser.
+#
+###############################################################################
 
 #mysql python docs at https://github.com/farcepest/MySQLdb1/blob/master/doc/user_guide.rst#mysqldb
 
@@ -12,6 +30,7 @@ from bs4 import BeautifulSoup
 
 base_img_dir='/Library/WebServer/Documents/joomla3/images/'
 gl_img_dir='/Users/kwh/temp/joomla3/geeklog-rv8-arvixe/public_html/rv8/images/articles/'
+hard_code_gl_img_dir = 'http://www.kilohotel.com/rv8/images/articles/'
 joomla_img_dir = 'images/'
 
 db=MySQLdb.connect(user='root',passwd="Yarmouth2",db="rv8_merge")
@@ -134,46 +153,62 @@ def parse_one(sid):
 
 
 def new_p(p, date, imgs, img_nums):
-    # move images to required directories, and create new <p> in format required by Joomla with modal extension
-    # convert the image tags from Geeklog [image1_left] to Joomla + Modal extension tags
-    # return the converted html text block
+    """move images to required directories, and create new <p> in format required by Joomla with modal extension
+    convert the image tags from Geeklog [image1_left] to Joomla + Modal extension tags
+    convert hard coded paths from Geeklog image directory to Joomla image directory
+    
+    Notes:
+    1. Articles published prior to 08 Jan 2005 had hard coded images in the html, with no thumbnails.
+    2. Articles published from 08 Jan 2005 to 13 Oct 2014 have hard coded images, with thumbnails
+    3. Articles published after 13 Oct 2014 have [image1_left] tags, and the images listed in the database
+    
+    return the converted html text block"""
+
+    # sample hard coded image with thumbnail <a href="images/20141013153858942_2_original.jpg" title="View unscaled image"><img width="299" height="199" align="left" src="images/20141013153858942_2.jpg" alt=""></a>
+    # joomla equivalent <a href="images/20141013/20141013153858942_2.jpg" class="modal" title="View unscaled image"><img style="margin: 5px; float: left;" src="images/20141013/20141013153858942_2.jpg"  alt=""/>
     
     img_codes=[]
     image_dir = base_img_dir + date
-    try:
-        os.mkdir(image_dir, 0o775)
-        for n, img_thumb in enumerate(imgs):
-            file_name_parts=img_thumb.split('.')
-            img = file_name_parts[0] + '_original.' + file_name_parts[1]
-            
-            joomla_thumb_full_path = base_img_dir + date + '/' + img_thumb
-            joomla_thumb = joomla_img_dir + date + '/' + img_thumb
-            
-            joomla_img_full_path = base_img_dir + date + '/' + img
-            joomla_img = joomla_img_dir + date + '/' + img
-            
-            shutil.copy2(gl_img_dir + img_thumb, joomla_thumb_full_path)
-            shutil.copy2(gl_img_dir + img, joomla_img_full_path)
-            
-            geeklog_code_left = '[image' + str(img_nums[n]) + '_left]'
-            geeklog_code_right = '[image' + str(img_nums[n]) + '_right]'
-            geeklog_code_no_align = '[image' + str(img_nums[n]) + ']'
-            
-            joomla_code_left='<a href="' + joomla_img + '" class="modal"><img style="margin: 5px; float: left;" src="' + joomla_thumb + '" /></a>'
-            joomla_code_right='<a href="' + joomla_img + '" class="modal"><img style="margin: 5px; float: right;" src="' + joomla_thumb + '" /></a>'
-            joomla_code_no_align='<a href="' + joomla_img + '" class="modal"><img style="margin: 5px;" src="' + joomla_thumb + '" /></a>'
-
-            p=p.replace(geeklog_code_left, joomla_code_left)
-            p=p.replace(geeklog_code_right, joomla_code_right)
-            p=p.replace(geeklog_code_no_align, joomla_code_no_align)
+    os.mkdir(image_dir, 0o775)
+    for n, img_thumb in enumerate(imgs):
+        file_name_parts=img_thumb.split('.')
+        img = file_name_parts[0] + '_original.' + file_name_parts[1]
         
-    except Exception as inst:
-        print type(inst)     # the exception instance
-        print inst           # __str__ allows args to be printed directly
+        joomla_thumb_full_path = base_img_dir + date + '/' + img_thumb
+        joomla_thumb = joomla_img_dir + date + '/' + img_thumb
+        
+        joomla_img_full_path = base_img_dir + date + '/' + img
+        joomla_img = joomla_img_dir + date + '/' + img
+        
+        shutil.copy2(gl_img_dir + img_thumb, joomla_thumb_full_path)
+        try:
+            shutil.copy2(gl_img_dir + img, joomla_img_full_path)
+        except IOError:
+            print "***", img, "thumbnail image not present"
+            
+        geeklog_code_left = '[image' + str(img_nums[n]) + '_left]'
+        geeklog_code_right = '[image' + str(img_nums[n]) + '_right]'
+        geeklog_code_no_align = '[image' + str(img_nums[n]) + ']'
+        
+        joomla_code_left='<a href="' + joomla_img + '" class="modal"><img style="margin: 5px; float: left;" src="' + joomla_thumb + '" /></a>'
+        joomla_code_right='<a href="' + joomla_img + '" class="modal"><img style="margin: 5px; float: right;" src="' + joomla_thumb + '" /></a>'
+        joomla_code_no_align='<a href="' + joomla_img + '" class="modal"><img style="margin: 5px;" src="' + joomla_thumb + '" /></a>'
+        
+        joomla_img_path = joomla_img_dir + date + "/"
+        p=p.replace(geeklog_code_left, joomla_code_left)
+        p=p.replace(geeklog_code_right, joomla_code_right)
+        p=p.replace(geeklog_code_no_align, joomla_code_no_align)
+        p=p.replace(hard_code_gl_img_dir, joomla_img_path)
+    
+    # except Exception as inst:
+    #     print type(inst)     # the exception instance
+    #     print inst           # __str__ allows args to be printed directly
 
     return p
     
 
 # parse_all()
-parse_one(20141216012305720) # new article
-# parse_one(20030507210918708) # old article with images
+# parse_one(20141216012305720) # new article
+# parse_one(20030507210918708) # old article with images in Geelog format
+# parse_one(2002102621491281) # very first article, with hard coded images
+parse_one(20141013153858942) #  sample with hard coded images and thumbnails
