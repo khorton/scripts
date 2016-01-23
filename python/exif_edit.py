c00e6b3c825801071d4f6d4101bfb4482a41a92b
Kevin Horton <kevin01@kilhotel.com>
2016-01-23 13:42:17 -0500

    merge

--- python/exif_edit.py
+++ python/exif_edit.py
@@ -27,12 +27,11 @@ year = '2015'
 
 conf = """
 # Set window title
-*.title = Create Modals Line for RV-8 Web Site
+*.title = Edit EXIF Date
 
 # Introductory text
 txt.type = text
-txt.default = This dialog creates a {modals} line to paste into an article \
-on the RV-8 web site in Joomla. The {modals} line is copied to the clipboard.
+txt.default = This dialog edits the date and description embedded in the EXIF of one or more images.
 txt.height = 100
 txt.width = 310
 txt.x = 340
@@ -41,13 +40,17 @@ txt.tooltip = This is an element of type “text”
 
 # Add a filesystem browser
 ob.type = openbrowser
-ob.label = Select full size image
+ob.label = Select image or directory
 ob.width=310
 #ob.tooltip = This is an element of type “openbrowser”
+# Add 2 checkboxes
+recursive.rely = -18
+recursive.type = checkbox
+recursive.label = Edit EXIF in all files in directory, recursively
 
 # Add a text field
 tf.type = textfield
-tf.label = Image caption
+tf.label = Image description
 tf.default = 
 tf.width = 310
 #tf.tooltip = This is an element of type “textfield”
@@ -61,14 +64,68 @@ tf.width = 310
 # rb.tooltip = This is an element of type “radiobutton”
 
 # Add a popup menu
-# pop.type = popup
-# pop.label = Example popup menu
-# pop.width = 310
-# pop.option = Popup menu item #1
-# pop.option = Popup menu item #2
-# pop.option = Popup menu item #3
-# pop.default = Popup menu item #2
-# pop.tooltip = This is an element of type “popup”
+day.type = popup
+day.label = Day
+day.width = 40
+day.option = 1
+day.option = 2
+day.option = 3
+day.option = 4
+day.option = 5
+day.option = 6
+day.option = 7
+day.option = 8
+day.option = 9
+day.option = 10
+day.option = 11
+day.option = 12
+day.option = 13
+day.option = 14
+day.option = 15
+day.option = 16
+day.option = 17
+day.option = 18
+day.option = 19
+day.option = 20
+day.option = 21
+day.option = 22
+day.option = 23
+day.option = 24
+day.option = 25
+day.option = 26
+day.option = 27
+day.option = 28
+day.option = 29
+day.option = 30
+day.option = 31
+day.default = 1
+day.tooltip = This is an element of type “popup”
+
+# Add a popup menu
+month.type = popup
+month.label = Month
+month.width = 40
+month.option = 1
+month.option = 2
+month.option = 3
+month.option = 4
+month.option = 5
+month.option = 6
+month.option = 7
+month.option = 8
+month.option = 9
+month.option = 10
+month.option = 11
+month.option = 12
+month.default = 1
+month.tooltip = This is an element of type “popup”
+
+# Add a text field
+year.type = textfield
+year.label = year
+year.default = 2015
+year.width = 60
+year.tooltip = Enter the year of the photo
 
 # Add 2 checkboxes
 # chk.rely = -18
@@ -119,21 +176,32 @@ image_path = result['ob']
 image_file = os.path.basename(image_path)
 image_comps = os.path.splitext(image_file)
 dirname = os.path.split(os.path.dirname(image_path))[1]
+day = result['day']
+month = result['month']
+year = result['year']
 
 caption=result['tf']
 if caption == '':
-    print('Caption is blank')
-    modal_line = '{modal images/' + year + '/' + dirname + '/' + image_file + '|width=2000|height=1333|group=mygallery-' + dirname + '}<img style="margin: 0px 5px 5px 0px; float: left;" src="images/' + year + '/' + dirname + '/thumbs/' + image_comps[0] + '.thumb' + image_comps[1] + '" />{/modal}'
-    
+    if result['recursive'] == '1':
+        if os.path.isdir(image_path):
+            command = '/usr/local/bin/exiftool −overwrite_original_in_place -r -AllDates="%s:%s:%s 00:00:00" "%s"' % (year, month, day, image_path)
+        else:
+            print('ERROR: Must select directory if recursive option is selected')
+            sys.exit()
+    else:
+        command = '/usr/local/bin/exiftool −overwrite_original_in_place -AllDates="%s:%s:%s 00:00:00" "%s"' % (year, month, day, image_path)
+        print(command)
 else:
-    modal_line = '{modal images/' + year + '/' + dirname + '/' + image_file + '|width=2000|height=1333|title=' + caption + '|group=mygallery-' + dirname + '}<img style="margin: 0px 5px 5px 0px; float: left;" src="images/' + year + '/' + dirname + '/thumbs/' + image_comps[0] + '.thumb' + image_comps[1] + '" alt="' + caption +'" />{/modal}'
-    # print("Caption is", caption)
-
-print(modal_line)
+    if result['recursive'] == '1':
+        if os.path.isdir(image_path):
+            command = '/usr/local/bin/exiftool −overwrite_original_in_place -r -AllDates="%s:%s:%s 00:00:00" -IPTC:Caption-Abstract="%s" %s' % (year, month, day, caption, image_path)
+        else:
+            print('ERROR: Must select directory if recursive option is selected')
+            sys.exit()
+    else:
+        command = '/usr/local/bin/exiftool −overwrite_original_in_place -AllDates="%s:%s:%s 00:00:00" -IPTC:Caption-Abstract="%s" %s' % (year, month, day, caption, image_path)
 
 # os.execl('/usr/bin/pbcopy', modal_line)
 
-outf = os.popen("pbcopy", "w")
-outf.write(modal_line)
-outf.close()
-
+print(command)
+os.system(command)
