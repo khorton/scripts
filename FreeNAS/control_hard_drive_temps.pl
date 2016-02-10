cd6f11ac4ac7c9c1185a8bc475a4193a063d0978
Kevin Horton <kevin01@kilohotel.com>
2016-02-10 06:25:06 -0500

    now select between Full, Standard and Optimal speed

--- FreeNAS/control_hard_drive_temps.pl
+++ FreeNAS/control_hard_drive_temps.pl
@@ -10,7 +10,7 @@ $hd_designator = "/dev/da";
 
 # edit nothing below this line
 
-$case = 0;
+$max_temp = 0;
 
 foreach $item (0..$number_of_hard_drives-1) {
   $command = "/usr/local/sbin/smartctl -A $hd_designator$item | grep Temp";
@@ -23,16 +23,24 @@ foreach $item (0..$number_of_hard_drives-1) {
   # grab last item from the output, which is the hard drive temperature
   $temp = "$vals[-1]\n";
 
-  # check for temperature greater than 39 deg C
-  if ($temp > 39) { $case = 1; }
+  # update maximum drive temperature
+  $max_temp = $temp if $temp > $max_temp;
 }
 
-if ($case == 1) {
+if ($max_temp > 39) {
   # at least one hard drive is 40 deg C or higher
   # set fan speed control to Full
   `ipmitool raw 0x30 0x45 0x01 0x01`
-} else {
-  # all hard drive temperatures are 39 deg C or cooler
+} 
+
+elsif ($max_temp == 39 ){
+  # maximum drive temperature is 39 deg C
+  # set fan speed to Standard
+  `ipmitool raw 0x30 0x45 0x01 0x00`
+}
+  
+else {
+  # all hard drive temperatures are 38 deg C or cooler
   # set fan speed control to Optimal
   `ipmitool raw 0x30 0x45 0x01 0x02`
 }
