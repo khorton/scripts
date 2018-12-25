f1b1da39c30d1a30bb4564b11c0ec4a70b0dbd26
Kevin Horton <kevin01@kilohotel.com>
2018-12-25 10:16:12 -0400

    working script

--- FreeNAS/backup_freenas_rsync.sh
+++ FreeNAS/backup_freenas_rsync.sh
@@ -2,4 +2,13 @@
 
 # Backup FreeNAS BigBertha server
 
-rsync -aHSX --delete --progress --stats --exclude-from '/root/rsync_exclude-list.txt' /mnt/main/ /mnt/bu01/ | tee -a /root/bu.log
+export MY_LOGDIR=/root
+
+MY_SCRIPT=`basename ${0}`
+MY_SYSTEM=`uname -n | cut -f1 -d'.'`
+CUR_DATE=`date +%Y\%m\%d`
+export MY_SCRIPT MY_SYSTEM CUR_DATE
+
+zpool scrub bu01
+
+rsync -aHSX --delete --progress --stats --exclude-from '/root/rsync_exclude-list.txt' /mnt/main/ /mnt/bu01/ | tee -a /root/bu01.log
