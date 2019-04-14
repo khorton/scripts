2ca2d455f380507775e42081bf5a8f82ebce7fd1
Kevin Horton <kevin01@kilohotel.com>
2019-04-14 05:56:36 -0700

    Update IP and from Fink to MacPorts

--- autossh-tunnel.sh
+++ autossh-tunnel.sh
@@ -2,4 +2,4 @@
 
 # setup ssh tunnel between Mac Mini and Linode server
 
-/sw/bin/autossh -M 5234 -N -f -T -R 22222:localhost:22 45.56.64.40
+/opt/local/bin/autossh -M 5234 -N -f -T -R 22222:localhost:22 45.33.33.133
