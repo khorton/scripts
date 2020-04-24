420145aa5b5b7502e8ece1a6018cf30af8c469f2
Kevin Horton <kevin01@kilohotel.com>
2020-04-23 20:18:23 -0700

    working python script to monitor Chase PPP page

--- python/web_change_monitor.py
+++ python/web_change_monitor.py
@@ -1,4 +1,8 @@
-#! /usr/bin/env python
+#! /opt/local/bin/python3.7
+
+patterns = ['April 23, 2020 6:30']
+app = '/Users/kwh/temp/Chase_PPP_Page_Update.app'
+# noapp = '/Users/kwh/temp/Chase_PPP_Page_NoUpdate.app'
 
 # Import requests (to download the page)
 import requests
@@ -9,17 +13,34 @@ from bs4 import BeautifulSoup
 # Import Time (to add a delay between the times the scape runs)
 import time
 
-# Import smtplib (to allow us to email)
-import smtplib
+import subprocess
 
 import re
 # set the url as VentureBeat,
 url = "https://www.chase.com/cares"
+
 # set the headers like we are a browser,
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 # download the homepage
 response = requests.get(url, headers=headers)
+
 # parse the downloaded homepage and grab all text, then,
 soup = BeautifulSoup(response.text, "lxml")
 updated_time=soup.find_all(string=re.compile("Updated"))
-print updated_time
+result = str(updated_time)
+
+for pattern in patterns:
+    print('Looking for "%s" in "%s" ->' % (pattern, result), end=' ')
+
+    if re.search(pattern,  result):
+        print('No page update')
+#         subprocess.call(
+# 		    ["/usr/bin/open", "-W", "-n", "-a", noapp]
+#     		)
+    else:
+        print('Page updated!!')
+        subprocess.call(
+		    ["/usr/bin/open", "-W", "-n", "-a", app]
+    		)
+
