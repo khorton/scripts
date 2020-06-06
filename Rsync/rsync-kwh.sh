8cc867083f539211657efc15ade540ba6c45aa4f
Kevin Horton <kevin01@kilohotel.com>
2020-06-05 18:49:45 -0700

    add --ignore-errors to rsync command.

--- Rsync/rsync-kwh.sh
+++ Rsync/rsync-kwh.sh
@@ -10,7 +10,7 @@
 # or 
 # while 1;do; sleep 60; if [ ps aux | grep rsync | wc -l -gt 2 ]; then; date | tee -a bu02.log; fi; done
 
-LOGDIR=~/logs/
+LOGDIR=~/logs
 LOG=$LOGDIR/BU$1.log
 echo $LOG
 
@@ -19,8 +19,8 @@ echo "Start at" `date` >> $LOG
 echo "—————————————————————————" >> $LOG
 
 #rsync -aHSX -vvv --delete-before --progress --stats --itemize-changes --exclude-from '/root/rsync_exclude-list.txt' --out-format="%t %f %b" --delete-excluded /mnt/iocage/ /mnt/$1/iocage/ | tee -a $LOG
-rsync -aHSx -v --delete-before --progress --stats --itemize-changes --exclude-from '/Users/kwh/sw_projects/git/scripts/Rsync/rsync_exclude-list.txt' --out-format="%t %f %b" --delete-excluded /Users/kwh/ /Volumes/KWH_$1/kwh/ | tee -a $LOG
-rsync -aHSx -v --delete-before --progress --stats --itemize-changes --exclude-from '/Users/kwh/sw_projects/git/scripts/Rsync/rsync_exclude-list.txt' --out-format="%t %f %b" --delete-excluded /opt/ /Volumes/KWH_$1/opt/ | tee -a $LOG
+rsync -aHSx -v --delete-before --progress --stats --itemize-changes --ignore-errors --exclude-from '/Users/kwh/sw_projects/git/scripts/Rsync/rsync_exclude-list.txt' --out-format="%t %f %b" --delete-excluded /Users/kwh/ /Volumes/KWH_$1/kwh/ | tee -a $LOG
+rsync -aHSx -v --delete-before --progress --stats --itemize-changes --ignore-errors --exclude-from '/Users/kwh/sw_projects/git/scripts/Rsync/rsync_exclude-list.txt' --out-format="%t %f %b" --delete-excluded /opt/ /Volumes/KWH_$1/opt/ | tee -a $LOG
 
 echo "—————————————————————————" >> $LOG
 echo "End at" `date` >> $LOG
