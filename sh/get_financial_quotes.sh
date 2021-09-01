35c71f390e0ba26c2d083cd39c9e4d8e71397aa0
Kevin Horton <kevin01@kilhotel.com>
2021-09-01 14:13:54 -0700

    update to call Gnucash-cli as the original command is deprecated

--- sh/get_financial_quotes.sh
+++ sh/get_financial_quotes.sh
@@ -5,5 +5,5 @@
 # /Users/kwh/Documents/Misc/Financial_etc/mutual_funds/mutual_buy_sell.py -s -b /Users/kwh/Documents/Misc/Financial_etc/mutual_funds/TD*.txt >>/Users/kwh/Documents/Misc/Financial_etc/mutual_funds/log.txt 2>&1
 echo "=================================================" >> /Users/kwh/Library/Logs/GnuCash_Quotes.log
 date | tee -a /Users/kwh/Library/Logs/GnuCash_Quotes.log
-/Applications/GnuCash/Gnucash.app/Contents/MacOS/Gnucash --add-price-quotes=/Users/kwh/Dropbox/GnuCash/Horton\ Finances.gnucash | tee -a /Users/kwh/Library/Logs/GnuCash_Quotes.log
+/Applications/GnuCash/Gnucash.app/Contents/MacOS/gnucash-cli --quotes get /Users/kwh/Dropbox/GnuCash/Horton\ Finances.gnucash | tee -a /Users/kwh/Library/Logs/GnuCash_Quotes.log
 
